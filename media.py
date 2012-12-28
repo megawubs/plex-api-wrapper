64b4bce67189c80f922a86943eba3f177a72e13d
megawubs <bwubs@me.com>
2012-12-28 08:41:04 +0100

    Added getMediaObject method, returns a object based on Media type

--- media.py
+++ media.py
@@ -1,3 +1,6 @@
+from episode import Episode
+from movie import Movie
+
 class Media(object):
 	"""Test class for media object"""
 	def __init__(self, element, server):
@@ -5,3 +8,15 @@ class Media(object):
 		self.server = server
 		self.video = element.find('./Video')
 		self.type = self.video.attrib['type']
+
+	def getMediaObject(self):
+		if self.type == "episode":
+			media = Episode(self.video, self.server)
+		elif self.type == "movie":
+			media = Movie(self.video, self.server)
+		if media:
+			return media
+		else:
+			return False
+
+			
